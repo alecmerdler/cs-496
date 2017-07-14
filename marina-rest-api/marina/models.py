0bb6b05aab73bf9548cecaf7af01638a00a2e0d7
Alec Merdler <alec.merdler@coreos.com>
2017-07-14 17:16:42 -0400

    added models

--- marina-rest-api/marina/models.py
+++ marina-rest-api/marina/models.py
@@ -3,4 +3,14 @@ from django.db import models
 
 class Boat(models.Model):
     id = models.CharField(max_length=30)
-    
+    name = models.CharField(max_length=30)
+    type = models.CharField(max_length=30)
+    length = models.PositiveIntegerField()
+    at_sea = models.BooleanField()
+
+
+class Slip(models.Model):
+    id = models.CharField(max_length=30)
+    number = models.PositiveIntegerField()
+    current_boat = models.ForeignKey(Boat)
+    arrival_date = models.CharField(max_length=9)
