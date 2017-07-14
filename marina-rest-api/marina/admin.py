adcbfbf3dd9084fe0a34690154044b8794df52f2
Alec Merdler <alec.merdler@coreos.com>
2017-07-14 19:40:52 -0400

    models

---
+++ marina-rest-api/marina/admin.py
@@ -0,0 +1,2 @@
+from models import Boat, Slip
+from django.contrib import admin
--- marina-rest-api/marina/views.py
+++ marina-rest-api/marina/views.py
@@ -1,5 +1,6 @@
 from django.views import View
 from django.http import JsonResponse
+from models import Boat, Slip
 import time
 
 
@@ -7,3 +8,19 @@ class MainView(View):
 
     def get(self, request):
         return JsonResponse({'current_timestamp': time.time()})
+
+
+class BoatView(View):
+    """
+    API endpoint for the boat resource.
+    """
+    def get(self, request):
+        boats = Boat.objects.all()
+
+        return JsonResponse([{'id': boat.id} for boat in boats])
+
+
+class SlipView(View):
+    """
+    API endpoint for the slip resource.
+    """
