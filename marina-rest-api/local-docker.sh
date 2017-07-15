f35b684c18df4aac95f0c3f8cf5e10c866b999c8
alecmerdler <merdlera@oregonstate.edu>
2017-07-14 21:01:56 -0400

    dockerfile

--- marina-rest-api/local-docker.sh
+++ marina-rest-api/local-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it --net=host -v $PWD:/usr/src/app quay.io/alecmerdler/marina-rest-api
+docker run -it --net=host -v $PWD:/usr/src/app quay.io/alecmerdler/marina-rest-api:latest
 
