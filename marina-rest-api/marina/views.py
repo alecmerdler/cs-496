0e6977115f5a510812ac50b77f3b335f585e6b14
alecmerdler <merdlera@oregonstate.edu>
2017-07-14 22:49:17 -0400

    /set_sail/

--- marina-rest-api/marina/views.py
+++ marina-rest-api/marina/views.py
@@ -2,6 +2,9 @@ from django.views import View
 from django.http import JsonResponse
 from models import Boat, Slip
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.response import Response
+from rest_framework import status
 from serializers import SlipSerializer, BoatSerializer
 import time
 
@@ -18,6 +21,18 @@ class BoatViewSet(ModelViewSet):
     """
     queryset = Boat.objects.all()
     serializer_class = BoatSerializer
+
+    @detail_route(methods=['post'])
+    def set_sail(self, request, pk=None):
+        boat = self.get_object()
+        boat.at_sea = True
+        boat.save()
+
+        for slip in Slip.objects.filter(current_boat=boat):
+            slip.current_boat = None
+            slip.save()
+
+        return Response(status=status.HTTP_200_OK)
 
 
 class SlipViewSet(ModelViewSet):
