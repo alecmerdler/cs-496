e607b042bd87a3ef0ba10b4cd5f5d32e83beebe1
Alec Merdler <alec.merdler@coreos.com>
2017-07-21 14:43:26 -0400

    a

--- oauth-implementation/index.js
+++ oauth-implementation/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const request = require('request');
 const path = require('path');
 
 
 const PORT = process.env.PORT || 8080;
+const oauthState = "secret";
 const app = express();
 
 /**
@@ -10,6 +12,26 @@ const app = express();
  */
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/oauth', (req, res) => {
+    const state = req.query['state'];
+    const code = req.query['code'];
+
+    if (state === oauthState) {
+        const requestBody = {
+            code: code,
+            client_id: `220231227847-fabrd2a3ogrstjha4ugl60626qp76vmb.apps.googleusercontent.com`,
+            client_secret: ``,
+            redirect_uri: ``,
+            grant_type: `authorization_code`,
+        };
+        request.post(`https://www.googleapis.com/oauth2/v4/token`, requestBody, (error, response, body) => {
+            console.log(body);
+        });
+    } else {
+
+    }
 });
 
 // Bootstrap the application
