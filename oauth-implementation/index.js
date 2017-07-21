1155a3d8ce94099850c7ad073cfc52f5b0db8f0c
Alec Merdler <alec.merdler@coreos.com>
2017-07-21 10:22:23 -0400

    p

--- oauth-implementation/index.js
+++ oauth-implementation/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+/**
+ * Route definitions.
+ */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+// Bootstrap the application
+app.listen(PORT, () => {
+    console.log(`App listening on ${PORT}!`);
 });
