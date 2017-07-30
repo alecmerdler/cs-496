fe7afa06133e6982ceb90fe4637e4d8b8804ab35
alecmerdler <merdlera@oregonstate.edu>
2017-07-29 20:54:30 -0400

    added infinite scrolling

--- android-ui/AndroidUI/app/src/main/java/edu/oregonstate/cs496/merdlera/androidui/main/infinite/InfiniteActivity.java
+++ android-ui/AndroidUI/app/src/main/java/edu/oregonstate/cs496/merdlera/androidui/main/infinite/InfiniteActivity.java
@@ -3,8 +3,15 @@ package edu.oregonstate.cs496.merdlera.androidui.main.infinite;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.github.pwittchen.infinitescroll.library.InfiniteScrollListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import edu.oregonstate.cs496.merdlera.androidui.R;
 import edu.oregonstate.cs496.merdlera.androidui.databinding.ActivityInfiniteBinding;
 
@@ -23,10 +30,15 @@ public class InfiniteActivity extends AppCompatActivity {
     };
 
     private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private int maxItemsPerRequest = imageSources.length;
+    private String[] visibleImageSources = imageSources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        layoutManager = new LinearLayoutManager(getApplicationContext());
 
         // Set up data binding
         ActivityInfiniteBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_infinite);
@@ -34,7 +46,19 @@ public class InfiniteActivity extends AppCompatActivity {
 
         // Initialize infinite list
         recyclerView = (RecyclerView) findViewById(R.id.infinite_recycler_view);
-        recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
-        recyclerView.setAdapter(new InfiniteImageAdapter(imageSources));
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(new InfiniteImageAdapter(visibleImageSources));
+        recyclerView.addOnScrollListener(new InfiniteScrollListener(maxItemsPerRequest, layoutManager) {
+            @Override
+            public void onScrolledToEnd(int firstVisibleItemPosition) {
+                // Add another set of image sources to list
+                visibleImageSources = new String[visibleImageSources.length + imageSources.length];
+                for (int i = 0; i < visibleImageSources.length; i++) {
+                    visibleImageSources[i] = imageSources[i % imageSources.length];
+                }
+
+                refreshView(recyclerView, new InfiniteImageAdapter(visibleImageSources), firstVisibleItemPosition);
+            }
+        });
     }
 }
