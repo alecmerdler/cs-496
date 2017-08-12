11f557068866a3cceb67b5955734c29ee85cfa70
Alec Merdler <alec.merdler@coreos.com>
2017-08-12 19:21:58 -0400

    gradle builds killin' me

--- android-oauth/AndroidOAuth/app/src/main/java/edu/oregonstate/cs496/merdlera/androidoauth/MainActivity.java
+++ android-oauth/AndroidOAuth/app/src/main/java/edu/oregonstate/cs496/merdlera/androidoauth/MainActivity.java
@@ -1,30 +1,35 @@
 package edu.oregonstate.cs496.merdlera.androidoauth;
 
-import android.accounts.Account;
 import android.app.Activity;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.widget.TextView;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import edu.oregonstate.cs496.merdlera.androidoauth.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
     private static int AUTHORIZE_DOMAINS = 123;
-    private Account authorizedAccount;
+    private GoogleApiClient googleApiClient;
+
+    private ActivityMainBinding binding;
+    private String userDisplayName;
+    private boolean signingOut = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.sign_in_button).setOnClickListener((view) -> authorizePlusDomainsAccess());
-    }
-
-    private void authorizePlusDomainsAccess() {
+        // Set up Google API client
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .requestScopes(new Scope("https://www.googleapis.com/auth/plus.me"))
@@ -32,12 +37,37 @@ public class MainActivity extends Activity {
                 .requestScopes(new Scope("https://www.googleapis.com/auth/plus.stream.write"))
                 .build();
 
-        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(this)
+        googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
+        // Set up data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setUserDisplayName(this.userDisplayName);
+        binding.setAuthorize((view) -> this.authorizePlusDomainsAccess());
+        binding.setSignOut((view) -> this.signOut());
+    }
+
+    private void authorizePlusDomainsAccess() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
         startActivityForResult(signInIntent, AUTHORIZE_DOMAINS);
+    }
+
+    private void signOut() {
+        signingOut = true;
+        new Thread(() -> {
+            ConnectionResult connectionResult = googleApiClient.blockingConnect();
+
+            Auth.GoogleSignInApi.signOut(googleApiClient)
+                    .setResultCallback((Status signOutStatus) -> {
+                        userDisplayName = null;
+                        ((TextView) findViewById(R.id.user_name)).setText("");
+                        signingOut = false;
+
+                        Auth.GoogleSignInApi.revokeAccess(googleApiClient)
+                                .setResultCallback((Status revokeAccessStatus) -> {});
+                    });
+        }).start();
     }
 
     @Override
@@ -48,7 +78,9 @@ public class MainActivity extends Activity {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if (result.isSuccess()) {
                 GoogleSignInAccount account = result.getSignInAccount();
-                authorizedAccount = account.getAccount();
+                ((TextView) findViewById(R.id.user_name)).setText(account.getDisplayName());
+//                userDisplayName = account.getDisplayName();
+//                binding.notifyPropertyChanged(BR._all);
             }
         }
     }
