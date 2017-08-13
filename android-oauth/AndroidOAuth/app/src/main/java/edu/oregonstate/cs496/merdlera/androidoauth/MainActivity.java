c1ff358cec17514b991b22d16d8ea1ec73353e3d
Alec Merdler <alec.merdler@coreos.com>
2017-08-12 23:42:38 -0400

    errors with 2-way databinding

--- android-oauth/AndroidOAuth/app/src/main/java/edu/oregonstate/cs496/merdlera/androidoauth/MainActivity.java
+++ android-oauth/AndroidOAuth/app/src/main/java/edu/oregonstate/cs496/merdlera/androidoauth/MainActivity.java
@@ -1,6 +1,6 @@
 package edu.oregonstate.cs496.merdlera.androidoauth;
 
-import android.app.Activity;
+import android.accounts.Account;
 import android.content.Intent;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
@@ -13,15 +13,30 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.common.api.Status;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.plusDomains.PlusDomains;
+import com.google.api.services.plusDomains.model.Acl;
+import com.google.api.services.plusDomains.model.Activity;
+import com.google.api.services.plusDomains.model.PlusDomainsAclentryResource;
 import edu.oregonstate.cs496.merdlera.androidoauth.databinding.ActivityMainBinding;
 
-public class MainActivity extends Activity {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends android.app.Activity {
 
     private static int AUTHORIZE_DOMAINS = 123;
     private GoogleApiClient googleApiClient;
+    private Account account;
+    private PlusDomains plusDomains;
 
     private ActivityMainBinding binding;
     private CurrentUser currentUser;
+    private String newPost = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +58,10 @@ public class MainActivity extends Activity {
         // Set up data binding
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setCurrentUser(this.currentUser);
+        binding.setNewPost(newPost);
         binding.setAuthorize((view) -> this.authorizePlusDomainsAccess());
         binding.setSignOut((view) -> this.signOut());
+        binding.setSharePost((view) -> new Thread(() -> createPost(newPost)));
     }
 
     private void authorizePlusDomainsAccess() {
@@ -55,7 +72,6 @@ public class MainActivity extends Activity {
     private void signOut() {
         new Thread(() -> {
             googleApiClient.blockingConnect();
-
             Auth.GoogleSignInApi.signOut(googleApiClient)
                     .setResultCallback((Status signOutStatus) -> {
                         currentUser = null;
@@ -74,28 +90,73 @@ public class MainActivity extends Activity {
         if (requestCode == AUTHORIZE_DOMAINS) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if (result.isSuccess()) {
-                GoogleSignInAccount account = result.getSignInAccount();
-//                ((TextView) findViewById(R.id.user_name)).setText(account.getDisplayName());
+                GoogleSignInAccount googleSignInAccount = result.getSignInAccount();
+                account = googleSignInAccount.getAccount();
+                GoogleAccountCredential credential =
+                        GoogleAccountCredential.usingOAuth2(
+                                MainActivity.this,
+                                Collections.singleton("https://www.googleapis.com/auth/contacts.readonly"));
+                credential.setSelectedAccount(account);
+                plusDomains = new PlusDomains.Builder(AndroidHttp.newCompatibleTransport(),
+                                                      JacksonFactory.getDefaultInstance(),
+                                                      credential).build();
+
                 currentUser = new CurrentUser();
-                currentUser.setDisplayName(account.getDisplayName());
+                currentUser.setDisplayName(googleSignInAccount.getDisplayName());
                 binding.setCurrentUser(currentUser);
+                new Thread(() -> listPosts()).start();
             }
         }
     }
 
     private void listPosts() {
+        try {
+            List<String> posts = new ArrayList<>();
+            List<Activity> activities = plusDomains.activities().list("me", "user").execute().getItems();
+            for (Activity activity : activities) {
+                posts.add(activity.getObject().getContent());
+            }
+            currentUser.setPosts(posts);
+        } catch (IOException ioe) {
 
+        }
+    }
+
+    private void createPost(String message) {
+        try {
+            PlusDomainsAclentryResource resource = new PlusDomainsAclentryResource();
+            resource.setType("domain");
+            List<PlusDomainsAclentryResource> aclEntries = new ArrayList<PlusDomainsAclentryResource>();
+            aclEntries.add(resource);
+            Acl acl = new Acl();
+            acl.setItems(aclEntries);
+            acl.setDomainRestricted(true);
+
+            Activity activity = new Activity().setObject(new Activity.PlusDomainsObject().setOriginalContent(message)).setAccess(acl);
+            plusDomains.activities().insert("me", activity).execute();
+        } catch (IOException ioe) {
+
+        }
     }
 
     public class CurrentUser extends BaseObservable {
         private String displayName;
+        private List<String> posts = new ArrayList<>();
         @Bindable
         public String getDisplayName() {
             return this.displayName;
         }
+        @Bindable
+        public List<String> getPosts() {
+            return this.posts;
+        }
         public void setDisplayName(String displayName) {
             this.displayName = displayName;
             notifyPropertyChanged(BR.displayName);
+        }
+        public void setPosts(List<String> posts) {
+            this.posts = posts;
+            notifyPropertyChanged(BR.posts);
         }
     }
 }
